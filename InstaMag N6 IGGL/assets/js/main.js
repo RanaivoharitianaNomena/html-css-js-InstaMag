5f8b680768162be845a387f0fbafd5f3b3cbdc6a
unknown <ranaivoharitiananomena@gmail.com>
2020-12-10 19:58:19 +0300

    finishing implent slide img js  #master

--- InstaMag N6 IGGL/assets/js/main.js
+++ InstaMag N6 IGGL/assets/js/main.js
@@ -8,18 +8,7 @@ const reply = (rep) => {
         res.className = "replies";
     }
 }
-
-// button = document.querySelector('input');
-//         button.addEventListener('click',updateButton);
-//         function updateButton() {
-//             if (button.value === 'Like') {
-//             button.value = '✓ Liked';
-//              } else {
-//         button.value = "Like";
-       
-//     }
-// }
-function updateButton() {
+ updateButton() {
                 if (this.value === 'Like') {
                 this.value = '✓ Liked';
                  } else {
@@ -29,4 +18,33 @@ function updateButton() {
 var button = document.getElementsByClassName("like_buton");
 for(var i = 0; i<button.length;i++){
     button[i].onclick = updateButton;
+} 
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 } 
